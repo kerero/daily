a56e49cf09f51620054d0fc60459acc613e0229e
Ori Kerer <orikerergil@gmail.com>
2023-04-15 17:23:00 +0300

    Added a model to represent app settings

---
+++ lib/src/models/settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+
+part 'settings.g.dart';
+
+@collection
+class Settings {
+  static const Id idConstant = 1;
+  final Id id = idConstant;
+
+  @enumerated
+  ThemeMode theme = ThemeMode.system;
+}
