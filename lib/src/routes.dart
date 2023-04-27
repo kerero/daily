cc0d2dbc87a17255fcc05b08f88c3c3a7023e40b
Ori Kerer <orikerergil@gmail.com>
2023-04-28 00:06:53 +0300

    Basic mobile app shell

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,5 +1,7 @@
 import 'package:daily/src/sample_feature/sample_item_details_view.dart';
 import 'package:daily/src/settings/settings_view.dart';
+import 'package:daily/src/utils.dart';
+import 'package:daily/src/views/app_shell_mobile.dart';
 import './views/app_shell_desktop.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -7,9 +9,9 @@ import 'package:go_router/go_router.dart';
 final router = GoRouter(routes: <RouteBase>[
   ShellRoute(
     builder: (BuildContext context, GoRouterState state, Widget child) {
-      return AppShellDesktop(
-        child: child,
-      );
+      return !isDesktop()
+          ? AppShellDesktop(child: child)
+          : AppShellMobile(child: child);
     },
     routes: [
       GoRoute(
---
+++ lib/src/views/app_shell_mobile.dart
@@ -0,0 +1,23 @@
+import 'package:collapsible_sidebar/collapsible_sidebar.dart';
+import 'package:flutter/material.dart';
+
+class AppShellMobile extends StatelessWidget {
+  final Widget child;
+
+  const AppShellMobile({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+          child: ListView(
+        children: const [Text("data"), Text("data 2")],
+      )),
+      body: child,
+    );
+  }
+}
